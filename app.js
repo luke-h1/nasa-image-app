b1c168a76c6644d4fd009231a26a1268d2f0be8d
luke-h1 <luke.howsam@yahoo.com>
2020-08-18 20:29:26 +0100

    thank fuck it's finally working...

--- app.js
+++ app.js
@@ -9,20 +9,40 @@ async function getData(e, searchTerm){
   try {   
       e.preventDefault();
       const searchTerm = inputValue.value;   
-      const API_URL  = `https://api.nasa.gov/planetary/apod?api_key=
-      `
+      const API_URL  = `https://api.nasa.gov/planetary/apod?api_key=GLHzhZqyb81Q01i0foEhOCpekFuluhEjne23uu3O
+      `; 
       // const API_URL = `https://images-api.nasa.gov/search?q=${searchTerm}`; 
       await fetch((`${API_URL}`)) 
         .then((res) => res.json()) 
         .then((res) => { 
-          console.log(res);  
-          imageResult.innerHTML = `<p>${res.explanation} <img src="${res.url}" /></p>`   
+          console.log(res); 
+          const arr = res; 
+          console.log(arr) 
+          for (const [date, explanation, hdurl] in arr) {
+            let output = `
+            <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="${arr.hdurl}" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">${arr.title}</h5>
+              <p class="card-text">${arr.explanation}</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+            
+            
+            
+            
+            
+            `; 
+            imageResult.innerHTML = output;
+          } 
+          for (const explanation in arr){
 
-        })
+          }
+          });
+  }catch(e){
 
-      } catch (e) {
-    console.log(e); 
+
   }
+
 } 
-
-
