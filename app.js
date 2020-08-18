d48ec13af482f518a49297e87f847cac29c922e9
luke-h1 <luke.howsam@yahoo.com>
2020-08-18 20:07:05 +0100

    getting proper response from API

--- app.js
+++ app.js
@@ -9,13 +9,15 @@ async function getData(e, searchTerm){
   try {   
       e.preventDefault();
       const searchTerm = inputValue.value;   
-      const API_URL  = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`
+      const API_URL  = `https://api.nasa.gov/planetary/apod?api_key=
+      `
       // const API_URL = `https://images-api.nasa.gov/search?q=${searchTerm}`; 
       await fetch((`${API_URL}`)) 
         .then((res) => res.json()) 
         .then((res) => { 
-          console.log(res);
-          let img = document.createElement('img');
+          console.log(res);  
+          imageResult.innerHTML = `<p>${res.explanation} <img src="${res.url}" /></p>`   
+
         })
 
       } catch (e) {
