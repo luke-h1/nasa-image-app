62df13e25b4eb725b0a26ead030492c5cc18121c
luke-h1 <luke.howsam@yahoo.com>
2020-08-18 19:55:39 +0100

    update

--- app.js
+++ app.js
@@ -9,10 +9,15 @@ async function getData(e, searchTerm){
   try {   
       e.preventDefault();
       const searchTerm = inputValue.value;   
-      const API_URL = `https://images-api.nasa.gov/search?q=${searchTerm}`; 
+      const API_URL  = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`
+      // const API_URL = `https://images-api.nasa.gov/search?q=${searchTerm}`; 
       await fetch((`${API_URL}`)) 
         .then((res) => res.json()) 
-        .then((res) => console.log(res.collection.items[0]))   
+        .then((res) => { 
+          console.log(res);
+          let img = document.createElement('img');
+        })
+
       } catch (e) {
     console.log(e); 
   }
