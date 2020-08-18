99d1bd45fca56390c09664e42828cb0d4102eebc
luke-h1 <luke.howsam@yahoo.com>
2020-08-18 19:11:21 +0100

    update

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const submitBtn = document.getElementById('btn'); 
 const inputValue = document.getElementById('nasaSearch'); 
-const imageDisplay = document.getElementById('images')
-
+const imageResult = document.getElementById('images'); 
 submitBtn.addEventListener('click', getData); 
 
 
@@ -13,12 +12,10 @@ async function getData(e, searchTerm){
       const API_URL = `https://images-api.nasa.gov/search?q=${searchTerm}`; 
       await fetch((`${API_URL}`)) 
         .then((res) => res.json()) 
-        .then((res) => console.log(res))  
-        addImagesToPage(images);   
-  } catch (e) {
+        .then((res) => console.log(res.collection.items[0]))   
+      } catch (e) {
     console.log(e); 
   }
 } 
-
 
 
