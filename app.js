bb18a8fed20cb7b02c19989e7c923a90a14ae4cb
luke-h1 <luke.howsam@yahoo.com>
2020-08-19 16:01:32 +0100

    change hdurl to url to prevent failures on nasa's API

--- app.js
+++ app.js
@@ -7,39 +7,65 @@ async function getData(e, searchTerm) {
     try {
         e.preventDefault();
         const searchTerm = inputValue.value;
-        const API_URL = `https://api.nasa.gov/planetary/apod?api_key=&date=${searchTerm}
+        const API_URL = `https://api.nasa.gov/planetary/apod?api_key={API_KEY}&date=${searchTerm}
       `;
         // const API_URL = `https://images-api.nasa.gov/search?q=${searchTerm}`;
         await fetch(`${API_URL}`)
             .then((res) => res.json())
             .then((res) => {
                 console.log(res);
-                const arr = res;
-                // console.log(arr);
-                for (const [date, explanation, hdurl] in arr) {
-                    let output = `
-                    <div class="wrapper"> 
-                        <div class="row ">
-                        <div class="col s10 m5">
-                          <div class="card">
-                            <div class="card-image">
-                              <img src="${arr.hdurl}">
-                              <span class="card-title">${arr.title}</span>
-                            </div>
-                            <div class="card-content">
-                              <p>${arr.explanation}</p>
-                            </div>
-                            <div class="card-action">
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                   </div>
-            `;
-                    imageResult.innerHTML = output;
-                }
+                // const arr = res;
+                let output = `
+                <div class="wrapper"> 
+                <div class="row ">
+                <div class="col s10 m5">
+                  <div class="card">
+                    <div class="card-image">
+                      <img src="${res.url}">
+                      <span class="card-title">${res.title}</span>
+                    </div>
+                    <div class="card-content">
+                      <p>${res.explanation}</p>
+                    </div>
+                    <div class="card-action">
+                    </div>
+                  </div>
+                </div>
+              </div>
+           </div>
+                `;
+                imageResult.innerHTML = output;
             });
-    } catch (e) {
-        console.log('error occured');
+    }catch(e){
+      console.log('error')
     }
 }
+
+//                 // console.log(arr);
+//                 for (const [date, explanation, url] in arr) {
+//                     let output = `
+//                     <div class="wrapper">
+//                         <div class="row ">
+//                         <div class="col s10 m5">
+//                           <div class="card">
+//                             <div class="card-image">
+//                               <img src="${arr.url}">
+//                               <span class="card-title">${arr.title}</span>
+//                             </div>
+//                             <div class="card-content">
+//                               <p>${arr.explanation}</p>
+//                             </div>
+//                             <div class="card-action">
+//                             </div>
+//                           </div>
+//                         </div>
+//                       </div>
+//                    </div>
+//             `;
+//                     imageResult.innerHTML = output;
+//                 }
+//             });
+//     } catch (e) {
+//         console.log('error occured');
+//     }
+// }
